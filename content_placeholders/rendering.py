868f79aa243afb10340b002b2381ee1ee8cc75dc
Diederik van der Boor <vdboor@edoburu.nl>
2011-12-09 12:43:31 +0100

    Smallfix - rendering code rename.
    renamed render_placeholder_items to render_content_items, because that
    is what the function actually does. Also removes the unneeded slotname
    parameter.

--- content_placeholders/rendering.py
+++ content_placeholders/rendering.py
@@ -14,15 +14,23 @@ def render_placeholder(request, placeholder, parent_object=None):
     # Filter the items both by placeholder and parent;
     # this mimics the behavior of CMS pages.
     items = placeholder.get_content_items(parent_object)
-    return render_placeholder_items(request, placeholder.slot, items)
+
+    if not items:
+        return "<!-- no items in placeholder '{0}' -->".format(placeholder.slot)
+    else:
+        return _render_items(request, items)
 
 
-def render_placeholder_items(request, slotname, items):
+def render_content_items(request, items):
     """
     Render a list of placeholder items as HTML string.
     """
     if not items:
-        return "<!-- no items in placeholder '{0}' -->".format(slotname)
+        return "<!-- no items in placeholder -->"
     else:
-        str = ''.join(contentitem.plugin._render_contentitem(contentitem, request) for contentitem in items)
-        return mark_safe(str)
+        return _render_items(request, items)
+
+
+def _render_items(request, items):
+    str = ''.join(contentitem.plugin._render_contentitem(contentitem, request) for contentitem in items)
+    return mark_safe(str)
