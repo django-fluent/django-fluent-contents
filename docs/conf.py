b6628bb6f1417f1b1b8d6a041c6bbc7edfe90616
Diederik van der Boor <vdboor@edoburu.nl>
2012-07-04 23:48:02 +0200

    Allow graphviz in docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,11 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'djangodummy.settings'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.intersphinx'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
