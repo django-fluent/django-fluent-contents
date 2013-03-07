8c3547dd39ede88fbaa2b613cb0acf5a0bac2d37
Diederik van der Boor <vdboor@edoburu.nl>
2013-03-07 12:40:56 +0100

    Cleanup: using @receiver syntax for comment signals

--- fluent_contents/plugins/commentsarea/models.py
+++ fluent_contents/plugins/commentsarea/models.py
@@ -1,9 +1,12 @@
 from django.contrib import comments
 from django.db import models
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 from django.db.models import signals
 from django.contrib.comments import signals as comments_signals
 from fluent_contents.models import ContentItem
+
+CommentModel = comments.get_model()
 
 
 class CommentsAreaItem(ContentItem):
@@ -27,17 +30,15 @@ def clear_commentarea_cache(comment):
 
 
 # Allow admin changes to invalidate the cache.
+@receiver(signals.post_save, sender=CommentModel)
+@receiver(signals.post_delete, sender=CommentModel)
 def _on_comment_changed(instance, **kwargs):
     clear_commentarea_cache(instance)
 
-CommentModel = comments.get_model()
-signals.post_save.connect(_on_comment_changed, sender=CommentModel)
-signals.post_delete.connect(_on_comment_changed, sender=CommentModel)
-
 
 # Allow frontend actions to invalidate the cache.
+@receiver(comments_signals.comment_was_posted)
+@receiver(comments_signals.comment_was_flagged)
 def _on_comment_posted(comment, **kwargs):
     clear_commentarea_cache(comment)
 
-comments_signals.comment_was_posted.connect(_on_comment_posted)
-comments_signals.comment_was_flagged.connect(_on_comment_posted)
