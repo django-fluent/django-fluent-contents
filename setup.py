3674b068d6c2332d1d1e1c478edd42a8b03ba2a3
Diederik van der Boor <vdboor@edoburu.nl>
2013-05-06 01:15:45 +0200

    Update setup.py version and read version from __init__.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-from os.path import dirname, join
-import sys, os
+from os import path
+import codecs
+import os
+import re
+import sys
+
 
 # When creating the sdist, make sure the django.mo file also exists:
 if 'sdist' in sys.argv:
@@ -13,9 +17,22 @@ if 'sdist' in sys.argv:
         os.chdir('..')
 
 
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name='django-fluent-contents',
-    version='0.9dev',
+    version=find_version('fluent_contents', '__init__.py'),
     license='Apache License, Version 2.0',
 
     install_requires=[
@@ -40,7 +57,7 @@ setup(
     ],
 
     description='A widget engine to display various content on Django pages',
-    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    long_description=read('README.rst'),
 
     author='Diederik van der Boor',
     author_email='opensource@edoburu.nl',
