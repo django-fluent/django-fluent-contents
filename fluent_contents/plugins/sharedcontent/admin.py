e760bf2dfe3f80f66bdb6733de385927de88603c
Diederik van der Boor <vdboor@edoburu.nl>
2013-09-11 00:35:17 +0200

    Sharedcontent: avoid overwriting the slug in new translations
    Also make the slug field less prominent,
    as it's a developer feature mainly.

--- fluent_contents/plugins/sharedcontent/admin.py
+++ fluent_contents/plugins/sharedcontent/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from fluent_contents.admin import PlaceholderFieldAdmin
 from fluent_contents.plugins.sharedcontent.models import SharedContent
 
@@ -11,15 +12,23 @@ class SharedContentAdmin(PlaceholderFieldAdmin):
 
     def get_prepopulated_fields(self, request, obj=None):
         # Needed instead of prepopulated_fields=.. for django-parler==0.9
-        return {
-            'slug': ('title',)
-        }
+        if obj is not None and obj.pk:
+            # Avoid overwriting the slug when adding a new language.
+            return {}
+        else:
+            return {
+                'slug': ('title',)
+            }
 
     # Using declared_fieldsets for Django 1.4, otherwise fieldsets= would work too.
     declared_fieldsets = (
         (None, {
-            'fields': ('title', 'slug', 'contents')
+            'fields': ('title', 'contents')
         }),
+        (_("Publication settings"), {
+            'fields': ('slug',),
+            'classes': ('collapse',),
+        })
     )
 
 admin.site.register(SharedContent, SharedContentAdmin)
