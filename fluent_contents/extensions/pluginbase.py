django-fluent/django-fluent-contents

commit 4cbb9041c93e1d59f50038b65b30a2fe65d40b69

sharedcontent: added optional `language` parameter to set explicit language

fluent_contents/rendering.py
@@ -21,6 +21,7 @@ import logging
 
 # This code is separate from the templatetags,
 # so it can be called outside the templates as well.
+from parler.utils.context import smart_override
 
 logger = logging.getLogger(__name__)
 
@@ -177,9 +178,12 @@ def _render_items(request, placeholder, items, parent_object=None, template_name
             except PluginNotFound as e:
                 output = ContentItemOutput(mark_safe(u'<!-- error: {0} -->\n'.format(str(e))))
             else:
-                # Plugin output is likely HTML, but it should be placed in mark_safe() to raise awareness about escaping.
-                # This is just like Django's Input.render() and unlike Node.render().
-                output = plugin._render_contentitem(request, contentitem)
+                # Always try to render the template in the ContentItem language.
+                # This makes sure that {% trans %} tags function properly if the language happens to be different.
+                with smart_override(contentitem.language_code):
+                    # Plugin output is likely HTML, but it should be placed in mark_safe() to raise awareness about escaping.
+                    # This is just like Django's Input.render() and unlike Node.render().
+                    output = plugin._render_contentitem(request, contentitem)
 
                 if appsettings.FLUENT_CONTENTS_CACHE_OUTPUT and plugin.cache_output and contentitem.pk:
                     contentitem.plugin.set_cached_output(placeholder_cache_name, contentitem, output)

fluent_contents/plugins/sharedcontent/templatetags/sharedcontent_tags.py
@@ -34,7 +34,7 @@ class SharedContentNode(BaseNode):
     """
     min_args = 1
     max_args = 1
-    allowed_kwargs = ('template',)
+    allowed_kwargs = ('language', 'template',)
 
 
     @classmethod
@@ -60,7 +60,16 @@ class SharedContentNode(BaseNode):
             except SharedContent.DoesNotExist:
                 return "<!-- shared content '{0}' does not yet exist -->".format(slot)
 
+        language_code = tag_kwargs.get('language') or None
         template_name = tag_kwargs.get('template') or None
+
+        if language_code:
+            sharedcontent.set_current_language(language_code)
+
+        return self.render_shared_content(request, sharedcontent, template_name)
+
+    def render_shared_content(self, request, sharedcontent, template_name):
+        # All parsing done, perform the actual rendering
         output = rendering.render_placeholder(request, sharedcontent.contents, sharedcontent, template_name=template_name, fallback_language=True)
         rendering.register_frontend_media(request, output.media)  # Need to track frontend media here, as the template tag can't return it.
         return output.html

commit 27f310d8a29776f3fd3e0ceb5d473c4e49e52e6d

Fix rendering fallback items in the current language instead of the saved database language
* Added `render_ignore_item_language` option
* Automatically ignore item language when ``cache_output_per_language`` is set.

This deals with fallback-language issues that
4cbb9041c93e1d59f50038b65b30a2fe65d40b69 introduced.

fluent_contents/extensions/pluginbase.py
@@ -171,6 +171,11 @@ class ContentPlugin(with_metaclass(PluginMediaDefiningClass, object)):
     #:
     #: * Display fallback content on pages, but still use `{% trans %}` inside templates.
     #: * Dynamically switch the language per request, and *share* content between multiple languages.
+    #:
+    #: .. note::
+    #:    This option doesn't have to be used for translated CMS pages,
+    #:    as each page has it's own :class:`~fluent_contents.models.ContentItem` objects.
+    #:    It's only needed for rendering the *same* item in different languages.
     cache_output_per_language = False
 
     #: .. versionadded: 1.0
@@ -183,6 +188,14 @@ class ContentPlugin(with_metaclass(PluginMediaDefiningClass, object)):
 
     #: The category to display the plugin at.
     category = None
+
+    #: ..versionadded:: 1.0
+    #: By default, the plugin is rendered in the :attr:`language_code` it's written in.
+    #: It can be disabled explicitly in case the content should be rendered language agnostic.
+    #: For fallback content or plugins that cache output per language, this flag will be ignored.
+    #:
+    #: See also: :attr:`cache_output_per_language`
+    render_ignore_item_language = False
 
     #: Alternative template for the view.
     ADMIN_TEMPLATE_WITHOUT_LABELS = "admin/fluent_contents/contentitem/admin_form_without_labels.html"
@@ -318,6 +331,9 @@ class ContentPlugin(with_metaclass(PluginMediaDefiningClass, object)):
 
         # Append language code
         if self.cache_output_per_language:
+            # NOTE: Not using self.language_code, but using the current language instead.
+            #       That is what the {% trans %} tags are rendered as after all.
+            #       The render_placeholder() code can switch the language if needed.
             user_language = get_language()
             if user_language not in self.cache_supported_language_codes:
                 user_language = 'unsupported'

fluent_contents/plugins/sharedcontent/content_plugins.py
@@ -28,7 +28,8 @@ class SharedContentPlugin(ContentPlugin):
     model = SharedContentItem
     form = SharedContentItemForm
     category = _('Advanced')
-    cache_output = False  # Individual items are cached, complete block not yet.
+    cache_output = False                # Individual items are cached, complete block not yet.
+    render_ignore_item_language = True  # Only switch for individual items, not this entire block.
 
     def render(self, request, instance, **kwargs):
         # Not using "template" parameter yet of render_placeholder().

fluent_contents/rendering.py
@@ -4,7 +4,6 @@ Contents is cached in memcache whenever possible, only the remaining items are q
 The templatetags also use these functions to render the :class:`~fluent_contents.models.ContentItem` objects.
 """
 import logging, os, six
-
 from future.builtins import str
 from django.conf import settings
 from django.core.cache import cache
@@ -13,6 +12,7 @@ from django.template.context import RequestContext
 from django.template.loader import render_to_string, select_template
 from django.utils.html import conditional_escape, escape
 from django.utils.safestring import mark_safe
+from django.utils.translation import get_language
 from fluent_contents import appsettings
 from fluent_contents.cache import get_rendering_cache_key
 from fluent_contents.extensions import PluginNotFound, ContentPlugin
@@ -177,9 +177,21 @@ def _render_items(request, placeholder, items, parent_object=None, template_name
             except PluginNotFound as e:
                 output = ContentItemOutput(mark_safe(u'<!-- error: {0} -->\n'.format(str(e))))
             else:
-                # Always try to render the template in the ContentItem language.
-                # This makes sure that {% trans %} tags function properly if the language happens to be different.
-                with smart_override(contentitem.language_code):
+                if plugin.render_ignore_item_language \
+                or (plugin.cache_output and plugin.cache_output_per_language):
+                    # Render the template in the current language.
+                    # The cache also stores the output under the current language code.
+                    #
+                    # It would make sense to apply this for fallback content too,
+                    # but that would be ambiguous however because the parent_object could also be a fallback,
+                    # and that case can't be detected here. Hence, better be explicit when desiring multi-lingual content.
+                    render_language = get_language()  # Avoid switching the content,
+                else:
+                    # Render the template in the ContentItem language.
+                    # This makes sure that {% trans %} tag output matches the language of the model field data.
+                    render_language = contentitem.language_code
+
+                with smart_override(render_language):
                     # Plugin output is likely HTML, but it should be placed in mark_safe() to raise awareness about escaping.
                     # This is just like Django's Input.render() and unlike Node.render().
                     output = plugin._render_contentitem(request, contentitem)