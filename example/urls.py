042a3aca22ed93e7c35ae7babac27d1fa89b7a3e
Diederik van der Boor <vdboor@edoburu.nl>
2015-12-30 13:54:45 +0100

    Fixed running the example app

--- example/urls.py
+++ example/urls.py
@@ -1,3 +1,4 @@
+import django
 from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
@@ -7,9 +8,17 @@ urlpatterns = [
     url(r'^admin/apps/tinymce/', include('tinymce.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^comments/', include('django.contrib.comments.urls')),
     #url(r'^forms/', include('form_designer.urls')),
 
     url(r'^articles/', include('article.urls')),
     url(r'', include('simplecms.urls')),
 ]
+
+if django.VERSION < (1, 7):
+    urlpatterns = [
+        url(r'^comments/', include('django.contrib.comments.urls')),
+    ] + urlpatterns
+else:
+    urlpatterns = [
+        url(r'^comments/', include('django_comments.urls')),
+    ] + urlpatterns
