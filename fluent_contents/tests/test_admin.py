f3a33472ee183ff2ad62e0368175f02b52f98774
Diederik van der Boor <vdboor@edoburu.nl>
2016-06-07 20:30:21 +0200

    Added test for django admin change page.

--- fluent_contents/tests/test_admin.py
+++ fluent_contents/tests/test_admin.py
@@ -1,15 +1,13 @@
-from pprint import pformat
-from django.contrib.admin import AdminSite
 from django.contrib.admin.templatetags.admin_urls import admin_urlname
-from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.messages.middleware import MessageMiddleware
-from django.core.urlresolvers import reverse
-from django.test import RequestFactory, override_settings
+from django.test import override_settings
+from django.utils import six
+
 from fluent_contents.models import Placeholder
 from fluent_contents.tests import factories
 from fluent_contents.tests.testapp.admin import PlaceholderFieldTestPageAdmin
-from fluent_contents.tests.testapp.models import PlaceholderFieldTestPage, RawHtmlTestItem
+from fluent_contents.tests.testapp.models import PlaceholderFieldTestPage, \
+    RawHtmlTestItem, TimeoutTestItem
 from fluent_contents.tests.utils import AdminTestCase
 
 
@@ -84,75 +82,97 @@ class AdminTest(AdminTestCase):
         rawhtmltestitem = placeholder.contentitems.all()[0]
         self.assertEqual(rawhtmltestitem.html, u'<b>foo</b>')
 
+    def test_change_page(self):
+        """
+        Testing how the change page works
+        """
+        # Create item outside Django admin
+        slot = PlaceholderFieldTestPage.contents.slot
+        page = PlaceholderFieldTestPage.objects.create(title='TEST2')
+        placeholder = Placeholder.objects.create_for_object(page, slot, role='m')
+        item1 = RawHtmlTestItem.objects.create_for_placeholder(placeholder, language_code='en', html='<b>foo</b>')
+
+        # Fetch the page
+        response = self.admin_get_change(page.pk)
+
+        # Collect all existing inputs
+        formdata = {}
+        formdata.update(response.context_data['adminform'].form.initial)
+        for inline_admin_formset in response.context_data['inline_admin_formsets']:
+            for boundfield in inline_admin_formset.formset.management_form:
+                formdata[boundfield.html_name] = boundfield.value()
+
+            for form in inline_admin_formset.formset.forms:
+                for boundfield in form:
+                    formdata[boundfield.html_name] = boundfield.value()
+
+        self.assertEqual(formdata, {
+            'title': u'TEST2',
+            'contents': 1,
+            'placeholder-fs-INITIAL_FORMS': 1,
+            'placeholder-fs-MIN_NUM_FORMS': 0,
+            'placeholder-fs-MAX_NUM_FORMS': 1000,
+            'placeholder-fs-TOTAL_FORMS': 1,
+            'placeholder-fs-0-DELETE': None,
+            'placeholder-fs-0-id': placeholder.pk,
+            'placeholder-fs-0-role': u'm',
+            'placeholder-fs-0-slot': u'field_slot1',
+            'placeholder-fs-0-title': u'Field Slot1',
+            'contentitem-INITIAL_FORMS': 1,
+            'contentitem-TOTAL_FORMS': 1,
+            'contentitem-MIN_NUM_FORMS': 0,
+            'contentitem-MAX_NUM_FORMS': 1000,
+            'contentitem-0-DELETE': None,
+            'contentitem-0-html': u'<b>foo</b>',
+            'contentitem-0-id': item1.pk,
+            'contentitem-0-parent_item': None,
+            'contentitem-0-parent_item_uid': 1,
+            'contentitem-0-placeholder': 1,
+            'contentitem-0-placeholder_slot': None,
+            'contentitem-0-polymorphic_ctype': item1.polymorphic_ctype_id,
+            'contentitem-0-sort_order': 1,
+        })
+
+        # Update the items, adding a new one
+        formdata.update({
+            'contentitem-TOTAL_FORMS': 2,
+            'contentitem-1-html': u'<b>bar</b>',
+            'contentitem-1-id': None,
+            'contentitem-1-parent_item': None,
+            'contentitem-1-parent_item_uid': 1,
+            'contentitem-1-placeholder': None,
+            'contentitem-1-placeholder_slot': slot,
+            'contentitem-1-polymorphic_ctype': ContentType.objects.get_for_model(TimeoutTestItem).pk,
+            'contentitem-1-sort_order': 2,
+        })
+
+        for key, value in six.iteritems(formdata):
+            if value is None:
+                formdata[key] = ''
+
+        response = self.admin_post_change(page.pk, formdata)
+
+        # Must have created two items
+        self.assertEqual([item.html for item in placeholder.contentitems.all()], [u'<b>foo</b>', u'<b>bar</b>'])
+        item2 = placeholder.contentitems.all()[1]
+        self.assertEqual(item2.placeholder, placeholder)
+        self.assertEqual(item2.parent, page)
+        self.assertEqual(item2.html, u'<b>bar</b>')
+
     def test_copy_language_backend(self):
         """
         Testing how the copy button works.
         It calls the ``get_placeholder_data_view`` function.
         """
-        self.admin_site.register(PlaceholderFieldTestPage, PlaceholderFieldTestPageAdmin)
-        modeladmin = self.admin_site._registry[PlaceholderFieldTestPage]
-
         page = factories.create_page()
         placeholder = factories.create_placeholder(page=page)
         item1 = factories.create_content_item(RawHtmlTestItem, placeholder=placeholder, html='AA')
         item2 = factories.create_content_item(RawHtmlTestItem, placeholder=placeholder, html='BB')
 
-        request = self._get_request(admin_urlname(page._meta, 'get_placeholder_data'))
-        data = modeladmin.get_placeholder_data_view(request, page.pk).jsondata
+        request = self.create_admin_request('post', admin_urlname(page._meta, 'get_placeholder_data'))
+        data = self.admin_instance.get_placeholder_data_view(request, page.pk).jsondata
         self.assertEqual(len(data['formset_forms']), 2)
         self.assertEqual(data['formset_forms'][0]['model'], 'RawHtmlTestItem')
         self.assertEqual(data['formset_forms'][0]['contentitem_id'], item1.pk)
         self.assertEqual(data['formset_forms'][1]['contentitem_id'], item2.pk)
         self.assertTrue('AA' in data['formset_forms'][0]['html'])
-
-    def _get_request(self, url, data=None):
-        request = self.factory.post(url, data=data or {})
-        request.csrf_processing_done = True
-        request.session = {}
-        request.user = self.admin_user
-        MessageMiddleware().process_request(request)
-        return request
-
-    def _post_add(self, modeladmin, formdata):
-        opts = modeladmin.opts
-        url = reverse('admin:{0}_{1}_add'.format(opts.app_label, opts.model_name))
-
-        # Build request
-        # Add properties which middleware would typically do
-        request = self._get_request(url, data=formdata)
-
-        # Make a direct call, circumvents login page.
-        return modeladmin.add_view(request)
-
-    def _get_management_form_data(self, modeladmin):
-        """
-        Return the formdata that the management forms need.
-        """
-        opts = modeladmin.opts
-        url = reverse('admin:{0}_{1}_add'.format(opts.app_label, opts.model_name))
-        request = self.factory.get(url)
-        request.user = self.admin_user
-
-        inline_instances = modeladmin.get_inline_instances(request)
-
-        forms = []
-        for inline_instance in inline_instances:
-            FormSet = inline_instance.get_formset(request)
-            formset = FormSet(instance=modeladmin.model())
-            forms.append(formset.management_form)
-
-        # In a primitive way, get the form fields.
-        # This is not exactly the same as a POST, since that runs through clean()
-        formdata = {}
-        for form in forms:
-            for boundfield in form:
-                formdata[boundfield.html_name] = boundfield.value()
-
-        return formdata
-
-    def _render_response(self, response):
-        if hasattr(response, 'render'):
-            # TemplateResponse
-            return u"== Context ==\n{0}\n\n== Response ==\n{1}".format(pformat(response.context_data), response.render().content)
-        else:
-            return response.content
