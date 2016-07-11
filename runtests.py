7b7bf14789e018ba6d691d31ec1f7439e04f80ec
Diederik van der Boor <vdboor@edoburu.nl>
2016-07-11 14:44:50 +0200

    Improve runtests to report actual Django/Python versions

--- runtests.py
+++ runtests.py
@@ -3,6 +3,15 @@ import sys
 import django
 from django.conf import settings, global_settings as default_settings
 from django.core.management import execute_from_command_line
+from os import path
+
+
+# Give feedback on used versions
+sys.stderr.write('Using Python version {0} from {1}\n'.format(sys.version[:5], sys.executable))
+sys.stderr.write('Using Django version {0} from {1}\n'.format(
+    django.get_version(),
+    path.dirname(path.abspath(django.__file__)))
+)
 
 if not settings.configured:
     if django.VERSION >= (1, 8):
@@ -43,6 +52,7 @@ if not settings.configured:
         DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
             }
         },
         INSTALLED_APPS = (
