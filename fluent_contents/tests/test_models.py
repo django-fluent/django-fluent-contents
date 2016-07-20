d1c3f195e455b926429aadf84cfd9fc51db2802f
Diederik van der Boor <vdboor@edoburu.nl>
2016-07-20 10:54:48 +0200

    Improve tests for older Django versions

--- fluent_contents/tests/test_models.py
+++ fluent_contents/tests/test_models.py
@@ -1,3 +1,4 @@
+import django
 from django.contrib.contenttypes.models import ContentType
 
 from fluent_contents.models import ContentItem
@@ -15,6 +16,7 @@ class ModelTests(AppTestCase):
         This would break the admin delete screen otherwise.
         """
         c = ContentType()
-        c.save()
+        if django.VERSION >= (1, 8):
+            c.save()
         a = ContentItem(polymorphic_ctype=c)
         self.assertEqual(str(a), "'(type deleted) 0' in 'None None'")
