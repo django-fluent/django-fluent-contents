535b07758a16dec2ce79781f19b34a96044b99d3
Diederik van der Boor <vdboor@edoburu.nl>
2020-01-05 12:57:42 +0100

    Update plugin_template to Python 3-only standards

--- fluent_contents/conf/plugin_template/models.py
+++ fluent_contents/conf/plugin_template/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.utils.six import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from fluent_contents.models import ContentItem
 
 
-@python_2_unicode_compatible
 class {{ model }}(ContentItem):
     """
     CMS plugin data model to ...
