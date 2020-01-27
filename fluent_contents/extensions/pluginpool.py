dc07f42d80116c1e42efcd0afae91b2843c2e95f
Diederik van der Boor <vdboor@edoburu.nl>
2020-01-27 14:24:37 +0100

    Fixed keeping a lock unneededly while scanning is complete
    This change little effect on the application itself, as the
    lock.aquire() was never touched again when the detection already took
    place. Since Django initializes the application first, this locking
    wasn't really needed anymore for web-based applications.

--- fluent_contents/extensions/pluginpool.py
+++ fluent_contents/extensions/pluginpool.py
@@ -232,13 +232,13 @@ class PluginPool(object):
         if self.detected:
             return
 
-        # In some cases, plugin scanning may start during a request.
-        # Make sure there is only one thread scanning for plugins.
-        self.scanLock.acquire()
-        if self.detected:
-            return  # previous threaded released + completed
-
         try:
+            # In some cases, plugin scanning may start during a request.
+            # Make sure there is only one thread scanning for plugins.
+            self.scanLock.acquire()
+            if self.detected:
+                return  # previous threaded released + completed
+
             import_apps_submodule("content_plugins")
             self.detected = True
         finally:
